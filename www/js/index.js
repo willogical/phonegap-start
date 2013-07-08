a3299a6703b28dfc0ef24b70bd233bc6015d6023
willogical <willogical@gmail.com>
2013-07-08 15:03:12 -0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,20 +33,21 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        var xhr = new XMLHttpRequest():
-            xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
-            // Response Handlers
-            xhr.onload = function () {
-                var repos = JSON.parse(xhr.response), i, reposHTML = "";
-                for (i = 0; i < repos.repositories.length; i++) {
-                    reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/"+ repos.repositores[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
-                }
-                document.getElementByID("allRepos").innderHTML = reposHTML;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
+         // Response handlers.
+        xhr.onload = function () {
+             var repos = JSON.parse(xhr.response), i, reposHTML = "";
+             for (i = 0; i < repos.repositories.length; i++) {
+               reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
+             }
+            document.getElementById("allRepos").innerHTML = reposHTML;
             };
-            
-            xhr.onerror = function () {
-                aler('error making the request');
-            };
+
+        xhr.onerror = function () {
+            alert('error making the request.');
+        };
+
         xhr.send();
 
     },
